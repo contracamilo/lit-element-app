c726e5fd63219f2c139f2d02382d4ed7db6f4106
Cami Viceroy <home@Camis-MacBook-Pro.local>
2024-06-09 17:49:01 +0200

    life cicle tests

--- src/views/About/about.ts
+++ src/views/About/about.ts
@@ -1,8 +1,32 @@
 import {customElement, property} from 'lit/decorators.js';
-import { LitElement, html, css } from "lit-element";
+import { LitElement, html, css } from "lit";
 
 @customElement('wc-about')
 class AboutPage extends LitElement {
+    constructor() {
+        super();
+        console.log('constructor');
+      }
+    
+      connectedCallback() {
+        super.connectedCallback();
+        console.log('connectedCallback');
+      }
+    
+      disconnectedCallback() {
+        super.disconnectedCallback();
+        console.log('disconnectedCallback');
+      }
+    
+      updated(changedProperties: Map<string | number | symbol, unknown>) {
+        super.updated(changedProperties);
+        console.log('updated');
+      }
+    
+      firstUpdated(changedProperties: Map<string | number | symbol, unknown>) {
+        super.firstUpdated(changedProperties);
+        console.log('firstUpdated');
+      }
         render(){
             return html`
                 <h1>About</h1>
